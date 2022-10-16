5494a0801a4165238aa402e9399abc747aa45ec9
Nelcione Dias <nelcionedias@gmail.com>
2022-10-16 19:30:00 -0300

    first commit

---
+++ src/main/java/com/com/nelcione/sisos/SisOsApplication.java
@@ -0,0 +1,13 @@
+package com.com.nelcione.sisos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisOsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/com/nelcione/sisos/SisOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.nelcione.sisos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
