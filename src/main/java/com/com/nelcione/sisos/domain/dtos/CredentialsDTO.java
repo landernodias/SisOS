64280e33891bb405a6dca247b7fec60c6be17472
Nelcione Dias <nelcionedias@gmail.com>
2022-11-09 13:38:25 -0300

    implement CredentialsDTO

---
+++ src/main/java/com/com/nelcione/sisos/domain/dtos/CredentialsDTO.java
@@ -0,0 +1,23 @@
+package com.com.nelcione.sisos.domain.dtos;
+
+public class CredentialsDTO {
+
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
